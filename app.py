ce49069543c79e7cb1c2788786a06ac839a33bb4
iEdgars <11521450+iEdgars@users.noreply.github.com>
2024-07-16 11:22:00 +0300

    Project selection & some mock data (to be removed later)

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import sqlite3
-from perfJira import confirm_connection
+import perfJira
+import requests
 
 # Set the page config
 st.set_page_config(
@@ -18,18 +19,34 @@ username = st.text_input("Jira Username")
 api_key = st.text_input("Jira API Key", type="password")
 jira_url = st.text_input("Jira URL")
 
+
 if st.button("Check Connection"):
-    if confirm_connection(jira_url, username, api_key):
+    if perfJira.confirm_connection(jira_url, username, api_key):
         st.success("Connection successful!")
     else:
         st.error("Connection failed. Please check your credentials and URL.")
 
-# if st.button("Fetch Data"):
-#     data = fetch_jira_issues(username, api_key, jira_url)
-#     if data is not None:
-#         write_df_to_db(data)
-#         st.success("Data fetched and saved successfully!")
+#auth for methods
+auth = (username, api_key)
 
-# if st.button("Load Data"):
-#     data = read_from_sqlite()
-#     st.dataframe(data)
+# Mock data
+mock_projects = [
+    {"id": "00001", "key": "ADOM", "name": "The ADOM Project"},
+    {"id": "00002", "key": "ADMT", "name": "DMT Project"},
+    {"id": "00003", "key": "AAAA", "name": "Project 1"}
+]
+
+projects = requests.get(f"{jira_url}/rest/api/latest/project", auth=auth).json()
+projects = mock_projects + projects
+project_options = {f"{project['key']}: {project['name']}": project for project in projects}
+selected_project_option = st.selectbox("Select a Project", [""] + list(project_options.keys()), index=0)
+
+if selected_project_option:
+    selected_project = project_options[selected_project_option]
+    selected_project_id = selected_project['id']
+    selected_project_key = selected_project['key']
+    selected_project_name = selected_project['name']
+    
+    
+
+
