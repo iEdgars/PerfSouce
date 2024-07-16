3ab8633930769d3c6a90fddab599d2dba6e30ea1
iEdgars <11521450+iEdgars@users.noreply.github.com>
2024-07-16 15:13:13 +0300

    added helps to inputs and selections

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import sqlite3
 import perfJira
 import requests
+import st_help
 
 # Set the page config
 st.set_page_config(
@@ -14,10 +15,10 @@ st.set_page_config(
 st.title("Project Performance Dashboard")
 
 # Streamlit UI for user inputs
-project = st.text_input("Project code")
-username = st.text_input("Jira Username")
-api_key = st.text_input("Jira API Key", type="password")
-jira_url = st.text_input("Jira URL")
+project = st.text_input("Project code", help=st_help.project_help)
+username = st.text_input("Jira Username", help=st_help.username_help)
+api_key = st.text_input("Jira API Key", type="password", help=st_help.api_key_help)
+jira_url = st.text_input("Jira URL", help=st_help.jira_url_help)
 
 
 if st.button("Check Connection"):
@@ -36,33 +37,35 @@ mock_projects = [
     {"id": "00003", "key": "AAAA", "name": "Project 1"}
 ]
 
-projects = requests.get(f"{jira_url}/rest/api/latest/project", auth=auth).json()
-projects = mock_projects + projects
-project_options = {f"{project['key']}: {project['name']}": project for project in projects}
-selected_project_option = st.selectbox("Select a Project", [""] + list(project_options.keys()), index=0)
+if project and username and api_key and jira_url:
+    projects = requests.get(f"{jira_url}/rest/api/latest/project", auth=auth).json()
+    projects = mock_projects + projects
+    project_options = {f"{project['key']}: {project['name']}": project for project in projects}
+    st.markdown('#')
+    selected_project_option = st.selectbox("Select a Project", [""] + list(project_options.keys()), index=0, help=st_help.selected_project_option_help)
 
-if selected_project_option:
-    selected_project = project_options[selected_project_option]
-    selected_project_id = selected_project['id']
-    selected_project_key = selected_project['key']
-    selected_project_name = selected_project['name']
-    
-    # Fetch all boards for the selected project
-    start_at = 0
-    max_results = 50
-    boards = []
-    while True:
-        response = requests.get(f"{jira_url}/rest/agile/latest/board", auth=auth, params={'startAt': start_at, 'maxResults': max_results}).json()
-        boards.extend(response['values'])
-        if response['isLast']:
-            break
-        start_at += max_results
+    if selected_project_option:
+        selected_project = project_options[selected_project_option]
+        selected_project_id = selected_project['id']
+        selected_project_key = selected_project['key']
+        selected_project_name = selected_project['name']
+        
+        # Fetch all boards for the selected project
+        start_at = 0
+        max_results = 50
+        boards = []
+        while True:
+            response = requests.get(f"{jira_url}/rest/agile/latest/board", auth=auth, params={'startAt': start_at, 'maxResults': max_results}).json()
+            boards.extend(response['values'])
+            if response['isLast']:
+                break
+            start_at += max_results
 
-    board_options = {f"{board['id']}: {board['name']}": board for board in boards if board.get('location', {}).get('projectKey') == selected_project_key}
-    selected_board_option = st.selectbox("Select a Board", [""] + list(board_options.keys()), index=0)
+        board_options = {f"{board['id']}: {board['name']}": board for board in boards if board.get('location', {}).get('projectKey') == selected_project_key}
+        selected_board_option = st.selectbox("Select a Board", [""] + list(board_options.keys()), index=0, help=st_help.selected_board_option_help)
 
-    if selected_board_option:
-        selected_board = board_options[selected_board_option]
-        selected_board_id = selected_board['id']
-        selected_board_name = selected_board['name']
+        if selected_board_option:
+            selected_board = board_options[selected_board_option]
+            selected_board_id = selected_board['id']
+            selected_board_name = selected_board['name']
 
---
+++ st_help.py
@@ -0,0 +1,30 @@
+#storing Help text seperatelly to simplify main code
+project_help = '''
+
+'''
+
+username_help = '''
+
+'''
+
+api_key_help = '''
+
+'''
+
+jira_url_help = '''
+
+'''
+
+selected_project_option_help = '''
+Select your Jira Project.\n
+Selections are based by Project Key and Project Name.\n 
+e.g. ADOM: Data Management Tool
+'''
+
+selected_board_option_help = '''
+Select Jira board to collect metrics for.\n 
+Selections are based by Board ID and Board name.\n 
+e.g. 123: DMT Scrum Board\n 
+\n 
+To collect metrics for entire Project, select "Whole Project"
+'''
