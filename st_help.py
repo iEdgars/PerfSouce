84a389748ade7c366fc4ddb7b16c8251c9317881
iEdgars <11521450+iEdgars@users.noreply.github.com>
2024-07-16 20:55:40 +0300

    more helps added

--- st_help.py
+++ st_help.py
@@ -1,18 +1,27 @@
 #storing Help text seperatelly to simplify main code
 project_help = '''
-
+Your project code in <EPAM> system.\n 
+it can be found under:\n 
+    https://projects.epam.com/dashboard/projects\n 
+\n 
+"Only where I am Key Staff" toggle can help to narrow down.
 '''
 
 username_help = '''
-
+Your username for Jira.\n 
+Often it's email address.
 '''
 
 api_key_help = '''
-
+Provide API Token for your user account.\n 
+API token can be created here:\n 
+https://id.atlassian.com/manage-profile/security/api-tokens
 '''
 
 jira_url_help = '''
-
+Start of URL when opening Jira.\n 
+Example:\n 
+https://epam.atlassian.net/
 '''
 
 selected_project_option_help = '''
