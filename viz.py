161dc72fde154772db6f052690c7e23360195143
iEdgars <11521450+iEdgars@users.noreply.github.com>
2024-08-12 10:53:37 +0300

    lead_cycle uncomment

--- viz.py
+++ viz.py
@@ -18,27 +18,27 @@ st.set_page_config(
 
 st.title('Time to Market')
 
-# for mt in ['Lead','Cycle']:
-#     for it in ['Epic', 'Story']:
-#         st.write(st_help.ttm_text(it, mt))
+for mt in ['Lead','Cycle']:
+    for it in ['Epic', 'Story']:
+        st.write(st_help.ttm_text(it, mt))
 
-#         if it == 'Epic':
-#             selection = True
-#         else:
-#             selection = False
+        if it == 'Epic':
+            selection = True
+        else:
+            selection = False
 
-#         df = vizDataJira.ttm_create_resolve_dates(epic_selection=selection)
-#         df2 = vizDataJira.ttm_first_inProgress_dates(epic_selection=selection)
-#         trans_df = vizDataJira.ttm_transform_and_join_dataframes(df, df2)
+        df = vizDataJira.ttm_create_resolve_dates(epic_selection=selection)
+        df2 = vizDataJira.ttm_first_inProgress_dates(epic_selection=selection)
+        trans_df = vizDataJira.ttm_transform_and_join_dataframes(df, df2)
 
-#         col1, col2 = st.columns([3,4])
-#         with col1:
-#             vizJira.plot_lead_cycle_bar_chart(trans_df, it, mt)
+        col1, col2 = st.columns([3,4])
+        with col1:
+            vizJira.plot_lead_cycle_bar_chart(trans_df, it, mt)
 
-#         with col2:
-#             vizJira.display_kpi_cards(trans_df, it, mt)
+        with col2:
+            vizJira.display_kpi_cards(trans_df, it, mt)
 
-#         st.divider()
+        st.divider()
 
 
 # Streamlit app
